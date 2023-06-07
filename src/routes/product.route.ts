fdc2b5b50818eea0411e8b0c514640273617734e
andrejvb <andre.jvb@gmail.com>
2023-06-07 13:33:55 -0300

    fifth requirement

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import nameValidation from '../middlewares/nameValidation';
+import priceValidation from '../middlewares/priceValidation';
 import productController from '../controllers/product.controller';
 
 const router = express.Router();
 
-router.post('/', productController.createProduct);
+router.post('/', nameValidation, priceValidation, productController.createProduct);
 router.get('/', productController.findAll);
 
 export default router;
